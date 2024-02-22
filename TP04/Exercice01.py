6623e26eeecf2efecdb7fd5787d27ec6f088ce62
MAFTOUH <omar.maftou0000@gmail.com>
2024-02-22 02:14:57 +0100

    Exercice 01 Done

--- TP04/Exercice01.py
+++ TP04/Exercice01.py
@@ -21,17 +21,25 @@ print(f"Moyenne des ventes du P3 : {np.round(np.mean(tab2D[ : , 2]) , 2)}")
 print('*' * 50)
 
 # L'indentification le mois ayant les ventes maximales pour chaque produit
-print(f"Le mois ayant les ventes maximales P1 : {np.argmax(tab2D[ : , 0])}")
-print(f"Le mois ayant les ventes maximales P2 : {np.argmax(tab2D[ : , 1])}")
-print(f"Le mois ayant les ventes maximales P3 : {np.argmax(tab2D[ : , 2])}")
+print(f"Le mois ayant les ventes maximales P1 : {np.argmax(tab2D[ : , 0]) + 1}")
+print(f"Le mois ayant les ventes maximales P2 : {np.argmax(tab2D[ : , 1]) + 1}")
+print(f"Le mois ayant les ventes maximales P3 : {np.argmax(tab2D[ : , 2]) + 1}")
 
 print('*' * 50)
 
 # Calculer la croissance mensuelle
-print(f"Taux de coissance du P1 : \n{( tab2D[ : , 0 ] - np.diff(tab2D[ : , 0 ] , prepend = 0 ) / np.diff(tab2D[ : , 0 ] , prepend = 0 ) ) * 100}")
-print(f"Taux de coissance du P2 : \n{( tab2D[ : , 1 ] - np.diff(tab2D[ : , 1 ] , prepend = 0 ) / np.diff(tab2D[ : , 1 ] , prepend = 0 ) ) * 100}")
-print(f"Taux de coissance du P3 : \n{( tab2D[ : , 0 ] - np.diff(tab2D[ : , 2 ] , prepend = 0 ) / np.diff(tab2D[ : , 2 ] , prepend = 0 ) ) * 100}")
+TauxCroissance = ( np.diff(tab2D , axis=0) / tab2D[:-1] ) * 100
+print(f"Taux de coissance du 3 Produits : \n{ np.int64(TauxCroissance) }")
 
 print('*' * 50)
 
 # Identifier et afficher le mois avec la plus forte croissance pour chaque produit
+print(f"le mois le plus forte croissance pour P1 : { np.argmax(TauxCroissance[ : , 0])+1 }")
+print(f"le mois le plus forte croissance pour P2 : { np.argmax(TauxCroissance[ : , 1])+1 }")
+print(f"le mois le plus forte croissance pour P3 : { np.argmax(TauxCroissance[ : , 2])+1 }")
+
+print('*' * 50)
+
+# La creation d'un tableau 1D contenant la somme des ventes pour chaque mois
+tab1D = np.array(np.sum(tab2D , axis=1))
+print(f"Somme des ventes pour chaque mois : \n{tab1D}")
